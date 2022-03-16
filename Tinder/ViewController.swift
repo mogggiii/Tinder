d1f4e1bdcb18e5669129ee2c8a6c942a9cdec605
mogggiii <58219015+mogggiii@users.noreply.github.com>
2022-03-16 19:30:24 +0700

    base ui Implementation

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	override func loadView() {
+		self.view = MainView()
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
