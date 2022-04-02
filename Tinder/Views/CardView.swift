11cf8d488a8ec746182eca0439e7da6516176514
mogggiii <mogggiii65@gmail.com>
2022-04-02 14:46:18 +0700

    //

--- Tinder/Views/CardView.swift
+++ Tinder/Views/CardView.swift
@@ -11,25 +11,25 @@ class CardView: UIView {
 	
 	// MARK: - CardViewModel
 	
-//	var cardViewModel: CardViewModel! {
-//		didSet {
-//			let imageName = cardViewModel.imageNames.first ?? ""
-//			imageView.image = UIImage(named: imageName)
-//			informationLabel.attributedText = cardViewModel.attributedString
-//			informationLabel.textAlignment = cardViewModel.textAlignment
-//			
-//			(0..<cardViewModel.imageNames.count).forEach { _ in
-//				let barView = UIView()
-//				barView.layer.cornerRadius = 2
-//				barView.backgroundColor = barDeselectedColor
-//				barsStackView.addArrangedSubview(barView)
-//			}
-//			
-//			barsStackView.arrangedSubviews.first?.backgroundColor = .white
-//			
-//			setupImageIndexObserver()
-//		}
-//	}
+	var cardViewModel: CardViewModel! {
+		didSet {
+			let imageName = cardViewModel.imageNames.first ?? ""
+			imageView.image = UIImage(named: imageName)
+			informationLabel.attributedText = cardViewModel.attributedString
+			informationLabel.textAlignment = cardViewModel.textAlignment
+			
+			(0..<cardViewModel.imageNames.count).forEach { _ in
+				let barView = UIView()
+				barView.layer.cornerRadius = 2
+				barView.backgroundColor = barDeselectedColor
+				barsStackView.addArrangedSubview(barView)
+			}
+			
+			barsStackView.arrangedSubviews.first?.backgroundColor = .white
+			
+			setupImageIndexObserver()
+		}
+	}
 	
 	/// encapsulation
 	fileprivate let barDeselectedColor = UIColor(white: 0, alpha: 0.1)
